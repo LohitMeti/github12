4268db66e7e73d2641294963706079b00fda01df
HEMANTH <HEMANTH@DESKTOP-I95JGMB.mshome.net>
2019-02-25 20:37:50 +0530

    HI

--- firstgit/src/pack1/firsttest.java
+++ firstgit/src/pack1/firsttest.java
@@ -1,5 +1,19 @@
 package pack1;
 
-public class firsttest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+
+
+public class firsttest {
+	static{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+	}
+  
+	@Test
+	public void test1(){
+		WebDriver drive=new ChromeDriver();
+		drive.close();
+	}
 }
